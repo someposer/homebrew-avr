18f6ec9e6c82c5b40da7ab8626d05919a0b1d389
David Watson <david@neonquill.com>
2012-09-30 14:16:03 -0400

    Added code to apply patches from Atmel tarball.

--- avr-binutils.rb
+++ avr-binutils.rb
@@ -1,11 +1,24 @@
 require 'formula'
 
+# Atmel distributes a complete tarball of patches.
+class AtmelPatches < Formula
+  url 'http://distribute.atmel.no/tools/opensource/Atmel-AVR-Toolchain-3.4.1/avr/avr-patches.tar.gz'
+  homepage 'http://www.atmel.com/tools/ATMELAVRTOOLCHAINFORLINUX.aspx'
+  sha1 '59a139a42c8dada06fa5e3ebbd3d37f8d16b0d11'
+end
+
 class AvrBinutils < Formula
   url 'http://ftp.gnu.org/gnu/binutils/binutils-2.22.tar.bz2'
   homepage 'http://www.gnu.org/software/binutils/binutils.html'
   md5 'ee0f10756c84979622b992a4a61ea3f5'
 
   option 'disable-libbfd', 'Disable installation of libbfd.'
+
+  def patches
+    mkdir buildpath/'patches'
+    AtmelPatches.new.brew { cp Dir['binutils/*'], buildpath/'patches' }
+    { :p0 => Dir[buildpath/'patches/*'] }
+  end
 
   def install
 
